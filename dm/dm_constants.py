d666317f87649e20d701791777390bdcd82337b1
artemisfowl <skb655952@gmail.com>
2020-05-08 22:05:48 +0530

    Adding constants for dm module

---
+++ dm/dm_constants.py
@@ -0,0 +1,10 @@
+'''
+	@file dm_constants.py
+	@module dm
+	@author sb
+	@date Fri, 08 May 2020 21:44:05 +0530
+	@brief module component containing the dm constants
+'''
+
+# configuration constants
+GAME_CONF_KEY = "gameconf"
