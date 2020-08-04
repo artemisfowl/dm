0316a766d50cbf718eba88d861f885820fc1c430
artemisfowl <skb655952@gmail.com>
2020-08-04 23:33:50 +0530

    Updated constants with debug and release dir option and value

--- dm/dm_constants.py
+++ dm/dm_constants.py
@@ -8,13 +8,19 @@
 
 # configuration constants
 GAME_CONF_KEY = "gameconf"
+
 LOGGER_SECTION = "logger"
 ENGINE_SECTION = "engine"
+
 LOGFILE_OPTION = "logfile"
 LOGDIR_OPTION = "logdir"
 LOGTOFILE_OPTION = "logtofile"
 LOGTOSTDIO_OPTION = "logtostdio"
 ENABLE_DEBUG_OPTION = "enabledebug"
+
+READ_RES_DIR_OPTION = "readresdir"
+READ_RES_DBG_DIR_OPTION = "debugdir"
+READ_RES_REL_DIR_OPTION = "releasedir"
 
 # default values
 LOGFILE_VALUE = "engine"
@@ -22,6 +28,9 @@ LOGDIR_VALUE = "logs"
 LOGTOFILE_VALUE = 1
 LOGTOSTDIO_VALUE = 0
 ENABLE_DEBUG_VALUE = 1
+READ_RES_DIR_VALUE = "./game-dev"
+READ_RES_DBG_DIR_VALUE = "debug"
+READ_RES_REL_DIR_VALUE = "release"
 
 # enable/disable constants
 ENABLE = 1
@@ -29,3 +38,6 @@ DISABLE = 0
 
 # game constants
 DEFAULT_FPS = 30
+
+# state identifiers
+MENU_STATE_ID = 1000
