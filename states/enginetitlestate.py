e706430cc37a770f64a4134e68d407ba0540a5d5
artemisfowl <skb655952@gmail.com>
2021-04-22 18:57:27 +0530

    Adding starting state

---
+++ states/enginetitlestate.py
@@ -0,0 +1,34 @@
+'''
+	@file titlestate.py
+	@module states
+	@author sb
+	@date Wed, 07 Apr 2021 08:44:14 +0530
+	@brief module to show the title of the game. This is the initial state in
+	which the game would be starting
+'''
+
+# standard libs/modules
+from uuid import uuid4
+
+# custom libs/modules
+from .state import State
+from .stateconstants import (ENGINE_BG_RESOURCE_DIR,
+		ENGINE_BG_ENGINE_TITLE_STATE_MUS)
+
+class EngineTitleState(State):
+	def __init__(self):
+		super(EngineTitleState, self).__init__()
+		self._set_uuid_()
+
+		# load the initial background images
+		self.set_dir(dirname=ENGINE_BG_RESOURCE_DIR)
+
+		# save the path of the music to be played in the background
+		self.bg_path = ENGINE_BG_ENGINE_TITLE_STATE_MUS
+
+	def _set_uuid_(self):
+		self.__uuid = uuid4()
+
+	def get_mus(self):
+		return self.bg_path
+
--- states/state.py
+++ states/state.py
@@ -1,20 +1,52 @@
 '''
 	@file state.py
-	@module dm
+	@module states
 	@author sb
 	@date Fri, 06 Nov 2020 00:42:26 +0530
-	@brief module component containing the menu state details
+	@brief module component containing the base state details
 '''
 
 # standard libs/modules
-from uuid import uuid4
-from abc import abstractmethod
+from abc import ABC, abstractmethod
+from glob import glob
+from os import sep
 
-class State:
+class Resource:
 	def __init__(self):
-		self._uuid = uuid4()
-		self._logger = None
+		super(Resource, self).__init__()
+		self.__dir = None
+		self.__files = None
+
+	def set_dir(self, dirname):
+		self.__dir = dirname
+		self.__files = self._load_resources()
+
+	def get_dir(self):
+		return self.__dir
+
+	def get_files(self):
+		return self.__files
+
+	def _load_resources(self):
+		return glob(f'{self.__dir}{sep}*.*')
+
+class State(ABC):
+	def __init__(self):
+		self.__resource = Resource()
+		self.__uuid = None
 
 	@abstractmethod
+	def _set_uuid_(self):
+		pass
+
 	def get_uuid(self):
-		return self._uuid
+		return self.__uuid
+
+	def set_dir(self, dirname):
+		self.__resource.set_dir(dirname)
+
+	def get_dir(self):
+		return self.__resource.get_dir()
+
+	def get_files(self):
+		return self.__resource.get_files()
